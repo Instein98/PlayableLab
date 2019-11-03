575c3a3bc5f6cd6d692860213683d623129eed9d
Instein98 <instein98@gmail.com>
2019-11-03 14:24:06 +0800

    Add robot animation

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace com.instein98.game{
+	public class PlayerAnimatorManager : MonoBehaviour {
+
+		private Animator animator;
+
+		#region Private Fields
+
+			[SerializeField]
+			private float directionDampTime = 0.25f;
+
+		#endregion
+
+		#region MonoBehaviour Callbacks
+			void Start () {
+				animator = GetComponent<Animator>();
+				if (!animator){
+					Debug.LogError("Missing Animator Component", this);
+				}
+			}
+			void Update () {
+				if (!animator)
+					return;
+				AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+				if (stateInfo.IsName("Base Layer.Run")){
+					if (Input.GetButtonDown("Fire2")){
+						animator.SetTrigger("Jump");
+					}
+				}
+				float h = Input.GetAxis("Horizontal");
+				float v = Input.GetAxis("Vertical");
+				if (v < 0){
+					v = 0;
+				}
+				animator.SetFloat("Speed", h * h + v * v);
+				animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+			}
+		#endregion
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace com.instein98.game{
+	public class PlayerManager : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
